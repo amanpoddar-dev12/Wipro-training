7d47f4b099d350fc9952a7a173fbce9cd989832a
amanpoddar-dev12 <amanpoddar775@gmail.com>
2025-07-21 13:11:18 +0530

    commit

---
+++ day3/Program.cs
@@ -0,0 +1,75 @@
+﻿// //Console is a predefined class and WriteLine is a method 
+
+// //Source Code -- HLL --- C#
+
+// // using System; // using is a keyword to import the namespaces (package)
+
+// // class Program
+// // {
+
+// //     // method is in pascal case (eg : AddOperation())
+// //     static void Main()
+// //     {
+// // int[] numbers = new int[5];
+// // numbers[0] = 34;
+// // numbers[1] = 45;
+// // numbers[2] = 67;
+// // Console.WriteLine(numbers[2]);
+
+
+// // //// See https://aka.ms/new-console-template for more information
+// //Console is a predefined class and WriteLine is a method 
+
+// //Source Code -- HLL --- C#
+
+// using System; // using is a keyword to import the namespaces (package)
+
+// class Program
+// {
+
+//     // method is in pascal case (eg : AddOperation())
+//     static void Main()
+//     {
+//         int noOfStudent = 5;
+//         string[] sName = new string[noOfStudent];
+
+//         Console.WriteLine("Enter the names of 5 students :");
+
+//         for (int i = 0; i < noOfStudent; i++)
+//         {
+
+//             Console.WriteLine("Enter name of student " + (i + 1));
+//             sName[i] = Console.ReadLine();
+//         }
+//         Console.WriteLine("Student Data :");
+//         for (int i = 0; i < 5; i++)
+//         {
+//             Console.WriteLine("The " + (i + 1) + "student name is : " + sName[i]);
+
+//         }
+
+//     }
+
+
+// }
+
+// //create a single dimensional array to store 5 student names by taking input from an user
+// //and then display
+
+// // for (int i = 0; i < 5; i++)
+// // {
+// //     Console.WriteLine("The array elements are " + numbers[i]);
+
+// // // }
+// // int numofstudent = 5;
+// // int[] numbers = new int [numofstudent];
+// // for (int i = 0; i < numbers.length; i++)
+// // {
+
+// // }
+
+
+// // }
+
+
+// // }
