700f245d0c8ec6c740c602def36f70626bdfe7e5
amanpoddar-dev12 <amanpoddar775@gmail.com>
2025-07-25 23:26:05 +0530

    commit

---
+++ Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+class Test
+{
+    public static void Main(String[] args)
+    {
+        List<int> studentName = new List<int>();
+        ArrayList subject = new ArrayList();
+        Stack<int> roll = new Stack<int>();
+
+        studentName.Add(20);
+        studentName.Add(30);
+        studentName.Add(50);
+
+        subject.Add(100);
+        subject.Add("kamims");
+        subject.Add("akiksim");
+
+
+        roll.Push(22);
+        roll.Push(33);
+        roll.Push(02);
+        roll.Push(9);
+        roll.Push(4);
+        Console.WriteLine(roll.Peek());
+        // Console.WriteLine(subject[0]);
+        // Console.WriteLine(subject[1]);
+        // Console.WriteLine(subject[2]);
+
+    }
+}
--- day7/MultiCastDelegate.cs
+++ day7/MultiCastDelegate.cs
@@ -1,6 +1,6 @@
 ﻿//// See https://aka.ms/new-console-template for more information
 //using System.Runtime.ExceptionServices;
-//using static ABC;
+
 
 
 ////Delegate - It is type safe that hold a reference to a method 
@@ -16,7 +16,8 @@
 ////Syntax   AccessSpecifier delegate void delegate_name(paramater_list)
 
 
-//class ABC {
+//class MultiCastDelegate
+//{
 
 //    // Delegate Declaration for add operations and Print 
 //    // parameterized Delegates responsible for method invocation at runtime
@@ -24,48 +25,58 @@
 
 //    public delegate void PrintDelegate(int result);
 
-//    public static void Main6(string[] args) {
+//    public static void Main9(string[] args)
+//    {
 
 //        //Assign Methods to delegates
 //        //Member functions are encapsulated using delegate
 //        Operations Add = AddNumbers;
 //        Operations Subtract = SubtractNumbers;
+
+
+
 //        /*int a ;
 //        // a =30;
 //        ABC a1 = new ABC();
 //        AddDelegate ad = AddNumbers;*/
 
-//        PrintDelegate print = PrintResult;
 
 //        // calling/using delegate
-//       int result1 = Add(10, 30);
-//        int result2 = Subtract(10, 30);
-//        Console.WriteLine("Enter your choice 1. for Addition and 2. for subtraction");
-//        int choice = int.Parse(Console.ReadLine());
+//        int sumResult = Add(10, 30);
+//        int diffResult = Subtract(10, 30);
 
+//        //Multi cast Delegate
+//        PrintDelegate print = PrintResult; // adding first page as printResult method
+//        print += PrintResultCalci; // adding second page as printresultcalci method
 
-//        switch (choice)
-//        {
-//            case 1:
-//                print(result1);
-//                break;
-//            case 2:
-//                print(result2);
-//                break;
-//        }
+//        Console.WriteLine("Sum of two numbers :");
+//        print(sumResult);
+//        Console.WriteLine("Difference of two numbers :");
+//        print(diffResult);
+//       // Console.WriteLine("Enter your choice 1. for Addition and 2. for subtraction");
+//      //  int choice = int.Parse(Console.ReadLine());
+
+        
+       
 
 //    }
 
 
-//   private static int AddNumbers(int x , int y)
+//    private static int AddNumbers(int x, int y)
 //    { return x + y; }
 
 //    private static int SubtractNumbers(int x, int y)
 //    { return x - y; }
 
 //    private static void PrintResult(int result)
-//    { 
-//        Console.WriteLine("The result is :" + result);
+//    {
+//        Console.WriteLine("The result of addition is :" + result);
+//    }
+
+//    private static void PrintResultCalci(int result)
+//    {
+//        result = result * 10;
+//        Console.WriteLine("after applying bonus :" + result);
 //    }
 //}
 
@@ -73,3 +84,5 @@
 
 //// create a delegate for a admin who is responsible for calculating the invoice(int tutionfess , int transportfees)
 ////and one more delegate which will print the invoice
+
+//// now create two print methods one for printing (Page1 )the actual invoice and second print method (page 2) for deduction of 100% from tution fees
