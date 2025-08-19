eb5e9c4c2521ffd7db1e9926303b74ca31dae3a7
amanpoddar-dev12 <amanpoddar775@gmail.com>
2025-08-19 15:32:11 +0530

    commit

---
+++ day25/day25/Data/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿
+using day25.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
+
+
+namespace day25.Data
+{
+    public class ProductContext:DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
