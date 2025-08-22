108559e998977128652cb9918e8059c55eed6f04
amanpoddar-dev12 <amanpoddar775@gmail.com>
2025-08-22 17:06:25 +0530

    Initial project structure for StudentManagement app
    Added all source files, configuration, migrations, assets, and dependencies for the StudentManagement ASP.NET Core MVC application. This includes controllers, models, views, solution and project files, database context, Entity Framework migrations, static assets, and supporting configuration for development and IIS Express.

---
+++ StudentManagement/StudentManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+
+
+using Microsoft.EntityFrameworkCore;
+using StudentManagement.Models.Entities;
+
+namespace StudentManagement.Data
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+
+}
---
+++ StudentManagement/StudentManagement/Models/Entities/Student.cs
@@ -0,0 +1,14 @@
+namespace StudentManagement.Models.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public bool Subscribed { get; set; }
+
+
+    }
+}
+
