56f1125bb4f9aa0bc793a71f2095277f535a8622
amanpoddar-dev12 <amanpoddar775@gmail.com>
2025-08-28 02:26:23 +0530

    add angular project

---
+++ EmployeeSystem/Models/AllModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+   public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+
+    }
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        [ForeignKey("dept")]
+        public int DeptId {  get; set; }
+        public Department? dept { get; set; }
+
+    }
+}
